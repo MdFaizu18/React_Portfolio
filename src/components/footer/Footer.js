a65d94c4a80f1e3d7a55885e3e2215a2d3951155
Md Faizu <136733703+MdFaizu18@users.noreply.github.com>
2024-09-10 16:57:05 +0530

    Update some small changes in Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -114,7 +114,7 @@ const Footer = () => {
           </li>
           <li>
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              API Reference
+              API References
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
           </li>
